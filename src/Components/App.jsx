f6cf49bb8cb4294f6617b81096c9fafc50797254
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-06 09:35:23 +0100

    Splash Init

---
+++ src/Components/App.jsx
@@ -0,0 +1,12 @@
+import Splash from "./Pages/splash";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <main>
+      <Splash />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Components/Pages/splash.jsx
@@ -0,0 +1,12 @@
+import FarmTo from "../img/farmtodoor.png";
+import "../CSS/styles.css";
+
+function Splash() {
+  return (
+    <main>
+      <img src={FarmTo} alt="Logo" className="ftd d-flex mx-auto" />
+    </main>
+  );
+}
+
+export default Splash;
