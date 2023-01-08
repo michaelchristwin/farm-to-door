b929c8e9a4d8ff6bac606eaa3d60c005edf66cb4
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-08 19:36:16 +0100

    999

--- src/Components/Pages/onboarding02.jsx
+++ src/Components/Pages/onboarding02.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import FarmTo from "../img/farmtodoor.png";
+import Car1 from "../img/car1.png";
 import "../CSS/styles.css";
 
 function Onboarding02() {
@@ -9,28 +9,18 @@ function Onboarding02() {
       <div className="car bg-dark">
         <Carousel>
           <Carousel.Item>
-            <img className="d-block w-100" src={FarmTo} alt="First slide" />
+            <img className="d-block w-100" src={Car1} alt="First slide" />
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100" src={FarmTo} alt="Second slide" />
+            <img className="d-block w-100" src="" alt="Second slide" />
 
             <Carousel.Caption>
               <h3>Second slide label</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            <img className="d-block w-100" src={FarmTo} alt="Third slide" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
             </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
