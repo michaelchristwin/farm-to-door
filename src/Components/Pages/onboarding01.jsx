3357f62c9ba0f41363e83a2fa32b40aab93c24a9
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-08 08:14:18 +0100

    :)

--- src/Components/Pages/onboarding01.jsx
+++ src/Components/Pages/onboarding01.jsx
@@ -7,7 +7,16 @@ function Onboarding01() {
       <div>
         <img src={Wrect} alt="svg" className="wrect mx-auto d-flex mt-3" />
       </div>
-      <p className="brand">FarmToDoor</p>
+      <div className="text-center">
+        <p className="brand">FarmToDoor</p>
+        <span className="d-block groc">The easiest way to get the</span>
+        <span className="d-block groc">
+          groceries you desire delivered to you.
+        </span>
+      </div>
+      <button className="btn" id="get">
+        Get Started
+      </button>
     </main>
   );
 }
