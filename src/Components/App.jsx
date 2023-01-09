9d40198d3ea8b7bba00896b5a2c2b6b1b225960c
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-09 15:35:21 +0100

    Added routing

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Onboarding02 from "./Pages/onboarding02";
 import Onboarding01 from "./Pages/onboarding01";
+import Login from "./Pages/login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path={BaseUrl} element={<Splash />} />
         <Route path={`${BaseUrl}/onboarding1`} element={<Onboarding01 />} />
         <Route path={`${BaseUrl}/onboarding2`} element={<Onboarding02 />} />
+        <Route path={`${BaseUrl}/login`} element={<Login />} />
       </Routes>
     </main>
   );
---
+++ src/Components/Pages/login.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <main>
+      <h1>999</h1>
+    </main>
+  );
+}
+
+export default Login;
