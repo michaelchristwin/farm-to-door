6afdb3505cc7b65d387d07ecffb15c935b3f97c8
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-17 12:12:36 +0100

    Added 4 products

---
+++ src/Components/JSX/product.jsx
@@ -0,0 +1,21 @@
+import Cart from "../img/cart.png";
+
+function Product(props) {
+  return (
+    <div className={`bg-white product d-block ${props.me}`}>
+      <img
+        src={props.img}
+        alt="Strawberry"
+        className={`${props.className} mx-auto d-block`}
+      />
+      <span className="product-name ms-2 d-block">{props.name}</span>
+      <span className="type d-block mt-2 ms-2">Fruit</span>
+      <div className="d-flex justify-content-between px-2">
+        <p className="price mt-3">Tsh{props.price}</p>
+        <img src={Cart} alt="cart" className="cart" />
+      </div>
+    </div>
+  );
+}
+
+export default Product;
--- src/Components/Pages/home.jsx
+++ src/Components/Pages/home.jsx
@@ -2,7 +2,11 @@ import FarmToDoor from "../img/farmtodoor.png";
 import "../CSS/styles.css";
 import Arrow from "../img/arrow.png";
 import Filter from "../img/filter.png";
+import Product from "../JSX/product";
 import Strawberry from "../img/strawberry.png";
+import Avacado from "../img/avacado.png";
+import Orange from "../img/orange.png";
+import Brocolli from "../img/brocolli.png";
 
 function Home() {
   return (
@@ -15,20 +19,39 @@ function Home() {
           <img src={Arrow} alt="Arrow" className="arrow mt-2 ms-1" />
         </div>
       </div>
-      <div className="py-3 d-flex justify-content-between">
-        <img src={Filter} alt="filter" className="filter ms-4" />
+      <div className="py-3 d-flex justify-content-center">
+        <img src={Filter} alt="filter" className="filter me-3" />
         <input
-          className="form-control border-0 text-center srch me-4"
+          className="form-control border-0 text-center srch me-1"
           type="search"
           placeholder="Search Grocery"
           aria-label="Search"
         />
       </div>
-      <div className="d-flex px-3">
-        <div className="bg-white product">
-          <img src={Strawberry} alt="Strawberry" className="strawberry" />
-          <p className="product-name">Strawberry</p>
-        </div>
+      <div className="d-flex px-3 justify-content-center">
+        <Product
+          img={Strawberry}
+          className="strawberry"
+          name="Strawberry"
+          price="10.6"
+          me="me-3"
+        />
+        <Product
+          img={Avacado}
+          className="avacado"
+          name="Avacado"
+          price="10.6"
+        />
+      </div>
+      <div className="d-flex px-3 justify-content-center mt-3">
+        <Product
+          img={Brocolli}
+          className="brocolli"
+          name="Broccoli"
+          price="10.6"
+          me="me-3"
+        />
+        <Product img={Orange} className="orange" name="Orange" price="1.60" />
       </div>
     </main>
   );
