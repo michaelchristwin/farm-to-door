91b5e928eb164be212c391037c6a488b34f6d03e
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-17 11:17:17 +0100

    Products init

--- src/Components/Pages/home.jsx
+++ src/Components/Pages/home.jsx
@@ -2,6 +2,7 @@ import FarmToDoor from "../img/farmtodoor.png";
 import "../CSS/styles.css";
 import Arrow from "../img/arrow.png";
 import Filter from "../img/filter.png";
+import Strawberry from "../img/strawberry.png";
 
 function Home() {
   return (
@@ -14,14 +15,20 @@ function Home() {
           <img src={Arrow} alt="Arrow" className="arrow mt-2 ms-1" />
         </div>
       </div>
-      <div className="mt-3">
-        <img src={Filter} alt="filter" className="filter float-start ms-4" />
+      <div className="py-3 d-flex justify-content-between">
+        <img src={Filter} alt="filter" className="filter ms-4" />
         <input
-          className="form-control border-0 text-center srch float-end me-3"
+          className="form-control border-0 text-center srch me-4"
           type="search"
           placeholder="Search Grocery"
           aria-label="Search"
         />
+      </div>
+      <div className="d-flex px-3">
+        <div className="bg-white product">
+          <img src={Strawberry} alt="Strawberry" className="strawberry" />
+          <p className="product-name">Strawberry</p>
+        </div>
       </div>
     </main>
   );
