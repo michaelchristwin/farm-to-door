ea1891804d4ff52caa6b1c8b92d41d5f01cf5ca3
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-18 10:28:07 +0100

    Added dropdown

--- src/Components/JSX/product.jsx
+++ src/Components/JSX/product.jsx
@@ -2,11 +2,11 @@ import Cart from "../img/cart.png";
 
 function Product(props) {
   return (
-    <div className={`bg-white product d-block ${props.me}`}>
+    <div className={`bg-white product d-block`}>
       <img
         src={props.img}
         alt="Strawberry"
-        className={`${props.className} mx-auto d-block`}
+        className={`${props.className} mx-auto d-block my-2`}
       />
       <span className="product-name ms-2 d-block">{props.name}</span>
       <span className="type d-block mt-2 ms-2">Fruit</span>
--- src/Components/Pages/home.jsx
+++ src/Components/Pages/home.jsx
@@ -3,10 +3,14 @@ import "../CSS/styles.css";
 import Arrow from "../img/arrow.png";
 import Filter from "../img/filter.png";
 import Product from "../JSX/product";
+import { Row, Col } from "react-bootstrap";
 import Strawberry from "../img/strawberry.png";
 import Avacado from "../img/avacado.png";
 import Orange from "../img/orange.png";
 import Brocolli from "../img/brocolli.png";
+import Tomato from "../img/tomato.png";
+import GreenBeans from "../img/greenbeans.png";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function Home() {
   return (
@@ -20,7 +24,16 @@ function Home() {
         </div>
       </div>
       <div className="py-3 d-flex justify-content-center">
-        <img src={Filter} alt="filter" className="filter me-3" />
+        <Dropdown>
+          <Dropdown.Toggle>
+            <img src={Filter} alt="filter" className="filter me-3" />
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
         <input
           className="form-control border-0 text-center srch me-1"
           type="search"
@@ -28,31 +41,50 @@ function Home() {
           aria-label="Search"
         />
       </div>
-      <div className="d-flex px-3 justify-content-center">
-        <Product
-          img={Strawberry}
-          className="strawberry"
-          name="Strawberry"
-          price="10.6"
-          me="me-3"
-        />
-        <Product
-          img={Avacado}
-          className="avacado"
-          name="Avacado"
-          price="10.6"
-        />
-      </div>
-      <div className="d-flex px-3 justify-content-center mt-3">
-        <Product
-          img={Brocolli}
-          className="brocolli"
-          name="Broccoli"
-          price="10.6"
-          me="me-3"
-        />
-        <Product img={Orange} className="orange" name="Orange" price="1.60" />
-      </div>
+      <Row className="mx-auto px-3">
+        <Col>
+          <Product
+            img={Strawberry}
+            className="strawberry"
+            name="Strawberry"
+            price="10.6"
+          />
+        </Col>
+        <Col>
+          <Product
+            img={Avacado}
+            className="avacado"
+            name="Avacado"
+            price="10.6"
+          />
+        </Col>
+      </Row>
+      <Row className="mx-auto px-3 mt-3">
+        <Col>
+          <Product
+            img={Brocolli}
+            className="brocolli"
+            name="Broccoli"
+            price="10.6"
+          />
+        </Col>
+        <Col>
+          <Product img={Orange} className="orange" name="Orange" price="1.60" />
+        </Col>
+      </Row>
+      <Row className="px-3 mx-auto mt-3">
+        <Col>
+          <Product img={Tomato} className="tomato" name="Tomato" price="5.60" />
+        </Col>
+        <Col>
+          <Product
+            img={GreenBeans}
+            className="green-beans"
+            name="Greenbeans"
+            price="5.60"
+          />
+        </Col>
+      </Row>
     </main>
   );
 }
