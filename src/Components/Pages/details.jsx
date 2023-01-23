6ff75cb817545c219dbf9d4cc161242a21ce6a85
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-23 16:43:50 +0100

    999

--- src/Components/Pages/details.jsx
+++ src/Components/Pages/details.jsx
@@ -6,6 +6,10 @@ import Star from "../img/5star.png";
 import { useState } from "react";
 import Plus from "../img/plus.png";
 import Minus from "../img/minus.png";
+import Strawberry from "../img/strawberry.png";
+import Product from "../JSX/product";
+import { Row, Col } from "react-bootstrap";
+import Brocolli from "../img/brocolli.png";
 
 function Details() {
   const [count, setCount] = useState(0);
@@ -30,16 +34,47 @@ function Details() {
       </section>
       <section className="px-3">
         <p className="fruit-name">Orange Fruit</p>
-        <div className="d-flex">
+        <div className="d-flex justify-content-between">
           <div className="d-flex">
             <img src={Star} alt="Rating" className="star" />
             <span className="ms-1 five">(5.0)</span>
           </div>
           <div className="d-flex">
-            <img src={Plus} alt="Add" className="contr" onClick={Add} />
-            <span className="mt-1">{count}</span>
-            <img src={Minus} alt="Minus" className="contr" onClick={Sub} />
+            <img src={Minus} alt="Add" className="contr" onClick={Sub} />
+            <span className="mt-1 mx-1 count">{count}</span>
+            <img src={Plus} alt="Minus" className="contr" onClick={Add} />
           </div>
+        </div>
+        <section className="mt-2">
+          <p className="descrip">Product Description</p>
+          <p className="descr-t">
+            The oranges are bright,easy to peel and have a sweet and
+            <span className="more"> More</span>
+          </p>
+          <p className="descrip">Nutrition and Ingredrients</p>
+          <p className="explore">Explore more related products</p>
+        </section>
+        <div className="px-2">
+          <Row>
+            <Col>
+              <Product
+                img={Strawberry}
+                className="strawberry"
+                name="Strawberry"
+                price="10.6"
+                type="Fruit"
+              />
+            </Col>
+            <Col>
+              <Product
+                img={Brocolli}
+                className="brocolli"
+                name="Broccoli"
+                price="10.6"
+                type="Vegetable"
+              />
+            </Col>
+          </Row>
         </div>
       </section>
     </main>
