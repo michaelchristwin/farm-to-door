d5dfc45538db5b64bfed2cbacf402d9983814827
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-23 12:19:23 +0100

    Added product counter

--- src/Components/Pages/details.jsx
+++ src/Components/Pages/details.jsx
@@ -3,8 +3,22 @@ import Like from "../img/like.png";
 import "../CSS/styles.css";
 import OrangeD from "../img/orange-d.png";
 import Star from "../img/5star.png";
+import { useState } from "react";
+import Plus from "../img/plus.png";
+import Minus from "../img/minus.png";
 
 function Details() {
+  const [count, setCount] = useState(0);
+  function Add() {
+    setCount((prevCount) => {
+      return prevCount + 1;
+    });
+  }
+  function Sub() {
+    setCount((prevCount) => {
+      return prevCount > 0 ? prevCount - 1 : 0;
+    });
+  }
   return (
     <main>
       <section className="top pt-4">
@@ -16,8 +30,16 @@ function Details() {
       </section>
       <section className="px-3">
         <p className="fruit-name">Orange Fruit</p>
-        <div>
-          <img src={Star} alt="Rating" className="star" />
+        <div className="d-flex">
+          <div className="d-flex">
+            <img src={Star} alt="Rating" className="star" />
+            <span className="ms-1 five">(5.0)</span>
+          </div>
+          <div className="d-flex">
+            <img src={Plus} alt="Add" className="contr" onClick={Add} />
+            <span className="mt-1">{count}</span>
+            <img src={Minus} alt="Minus" className="contr" onClick={Sub} />
+          </div>
         </div>
       </section>
     </main>
