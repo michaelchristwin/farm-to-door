b4929053b6c496619f2391e499440b3f759eeaa4
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-26 10:32:03 +0100

    finished cart page

--- src/Components/Pages/cart.jsx
+++ src/Components/Pages/cart.jsx
@@ -2,6 +2,7 @@ import "../CSS/styles.css";
 import BackG from "../img/back-g.png";
 import CartItem from "../JSX/CartItem";
 import CartList from "../JSX/CartList";
+import NavBar from "../JSX/Navbar";
 
 function Cart() {
   const ToBuy = CartList.map((item) => {
@@ -21,14 +22,16 @@ function Cart() {
         <img src={BackG} alt="Back" className="back-g" />
         <p className="cart-t mt-2 ms-2">My Cart Details</p>
       </div>
-      <section className="the-cart">
+      <section className="the-cart mb-5">
         <div className="d-flex justify-content-center pt-2">
           <button className="cart-b btn border">All Order</button>
           <button className="cart-b btn border ms-1">Pending</button>
           <button className="cart-b btn border ms-1">Processing</button>
         </div>
         {ToBuy}
+        <button className="btn checkout d-block mx-auto mt-5">Checkout</button>
       </section>
+      <NavBar />
     </main>
   );
 }
