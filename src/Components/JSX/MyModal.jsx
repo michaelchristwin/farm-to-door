d91d37e238dbedec9c703bbe8af6799a043d1b0b
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-28 04:00:03 +0100

    Init modal

--- src/Components/JSX/MyModal.jsx
+++ src/Components/JSX/MyModal.jsx
@@ -1,5 +1,6 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import "../CSS/styles.css";
 
 function MyVerticallyCenteredModal(props) {
   return (
@@ -9,13 +10,16 @@ function MyVerticallyCenteredModal(props) {
       aria-labelledby="contained-modal-title-vcenter"
       centered
     >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Modal heading
-        </Modal.Title>
-      </Modal.Header>
+      <div className="d-flex justify-content-between px-3 mt-2">
+        <p className="top-mod">Cart Total</p>
+        <p className="top-mod">TSH 11.52</p>
+      </div>
+      <p className="d-block ms-3 young">9000 Young Street store</p>
       <Modal.Body>
-        <h4>Centered Modal</h4>
+        <div className="pd border d-flex mx-auto">
+          <button className="btn-pd btn">Pickup</button>
+          <button className="btn-pd btn">Delivery</button>
+        </div>
         <p>
           Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
           dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
