25866902e40d2a5923115ec7a000f3552ab8d79e
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-01-31 05:34:16 +0100

    Init offers pagr

--- src/Components/Pages/offers.jsx
+++ src/Components/Pages/offers.jsx
@@ -2,6 +2,8 @@ import BackG from "../img/back-g.png";
 import NavBar from "../JSX/Navbar";
 import "../CSS/styles.css";
 import Pumpkin from "../img/pumpkin.png";
+import Tomato from "../img/tomato.png";
+import Orange from "../img/orange-d.png";
 
 function Offers() {
   return (
@@ -15,9 +17,27 @@ function Offers() {
           <div className="ms-3 me-1 mt-4">
             <span className="bf-text d-block text-nowrap">Black Friday</span>
             <span className="bf-text">offer</span>
-            <button className="check-now btn">Check now</button>
+            <button className="check-now btn mt-2">Check now</button>
           </div>
-          <img src={Pumpkin} alt="" className="pumpkin me-1 mt-3" />
+          <img src={Pumpkin} alt="pumpkin" className="pumpkin me-1 mt-3" />
+        </div>
+        <p className="avail mt-3">Available until 24 July 2021</p>
+        <div className="d-flex tomato-o mx-auto mt-3">
+          <div className="ms-3 me-1 mt-4">
+            <span className="bf-text d-block text-nowrap">Tomato</span>
+            <span className="bf-text">40% off</span>
+            <button className="check-now btn mt-2">Buy now</button>
+          </div>
+          <img src={Tomato} alt="tomato" className="pumpkin me-1 mt-3" />
+        </div>
+        <p className="avail mt-3">Available until 24 July 2021</p>
+        <div className="d-flex b-friday mx-auto mt-3">
+          <div className="ms-3 me-1 mt-4">
+            <span className="bf-text d-block text-nowrap">Orange</span>
+            <span className="bf-text">4% off</span>
+            <button className="check-now btn mt-2">Check now</button>
+          </div>
+          <img src={Orange} alt="orange" className="pumpkin me-1 mt-3" />
         </div>
         <NavBar />
       </section>
